3f4bf848e5fdfbeed426ec3cb3b019062f3eed72
James Priest <james-priest@outlook.com>
2018-12-25 08:58:41 -0800

    feat: Add Hello World component

---
+++ app/hello-world.js
@@ -0,0 +1,12 @@
+// formatted with Prettier
+var React = require("react");
+var ReactDOM = require("react-dom");
+require('./hello-world.css')
+
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
