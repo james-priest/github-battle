c0b1c4d6a354a55644b7914b2d5b3c81bf0be3fd
James Priest <james-priest@outlook.com>
2019-01-04 16:07:38 -0800

    feat: Add install-dev-env script; update entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
-  // entry: './app/hello-world.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
