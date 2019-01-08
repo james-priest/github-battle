4d8c0eeab22f13d5d1a214c5453d76d4d72f0b3b
James Priest <james-priest@outlook.com>
2019-01-08 04:19:09 -0800

    chore: update ESLint to use ES5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,53 @@
 module.exports = {
+  "root": true,
   "env": {
     "browser": true,
-    "es6": true,
+    "es6": false,
     "node": true
   },
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true
     },
-    "ecmaVersion": 2018,
+    "ecmaVersion": 5,
     "sourceType": "module"
   },
   "extends": [
-    "airbnb",
+    // "airbnb",
+    // "plugin:es5/no-es2015", // too restrictive
+    "airbnb-es5",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
     "prettier",
     "prettier/react",
   ],
   "plugins": [
+    "es5",
     "react",
     "jsx-a11y",
     "prettier"
   ],
   "rules": {
-    "func-names": "off",
-    "linebreak-style": "off",
-    "no-console": "off",
-    "prefer-const": [
-      "error",
-      {
-        "destructuring": "all",
-      }
-    ],
+    // off=0, warn=1, error=2
+    "es5/no-arrow-functions": 2,
+    "es5/no-block-scoping": 2,
+    "es5/no-destructuring": 2,
+    "es5/no-rest-parameters": 2,
+    "es5/no-spread": 2,
+    "es5/no-modules": 2,
+    "func-names": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "linebreak-style": 0,
+    "no-console": 0,
+    // "prefer-const": [
+    //   2,
+    //   {
+    //     "destructuring": "all",
+    //   }
+    // ],
     "prettier/prettier": ["error"],
-    "react/destructuring-assignment": 1,
+    "react/destructuring-assignment": 0,
     "react/jsx-filename-extension": [
       1,
       {
@@ -44,7 +57,7 @@ module.exports = {
         ]
       }
     ],
-    "react/no-multi-comp": 1,
+    "react/no-multi-comp": 0,
     "react/prefer-stateless-function": 0,
     "react/prop-types": 0,
   }
