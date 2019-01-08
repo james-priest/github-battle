76de1c8fd8ba5d5f73abec25daf2a2ed3ffec0c3
James Priest <james-priest@outlook.com>
2019-01-08 04:21:03 -0800

    fix: Default to es5 syntax

--- app/index.js
+++ app/index.js
@@ -1,15 +1,13 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 require('./index.css');
 
 // COMPONENTS CONSIST OF 3 THINGS
-// state
-// lifecycle events
-// UI
+//  - State
+//  - Lifecycle events
+//  - UI
 
-// Not a violation of SEPARATION OF CONCERNS
-// The concern is at the component level, not the technology level
 class App extends React.Component {
   render() {
     return <h1 className="App">Hello React!</h1>;
